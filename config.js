edb5c78a4701755fdb8f9db0fbb05a92c600fdec
bangph <bangph1610.work@gmail.com>
2024-03-23 14:51:38 +0700

    demo

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    introTitle: 'Chào công chúa!',
+    introDesc: `Cũng lâu rồi không được ngồi tâm sự gì với em 
+    nhỉ😘😘. Nghĩ đến em mà muốn nổ tung cái đầu của anh hihi`,
+    btnIntro: 'Đi tiếp nhé',
+    title: '😘Gửi công chúa PHAN MINH NGỌC😘',
+    desc: 'Lâu lắm rồi chả thấy được nụ cười trên môi của em mà nhớ quá chời á... ',
+}
