8d147968befd12bed58890b5fa937e6caa823fd2
Phạm Bằng <phambang2505@gmail.com>
2024-09-13 23:30:16 +0700

    update_code

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/2022_05_30_08_54_IMG_3542.JPG',
+        imageUrl: 'img/imageA.JPG',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
